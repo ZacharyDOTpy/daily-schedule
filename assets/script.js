e65e62616b60a7f10b2f681cfcbb08520e5b7f26
Zach <zach.Brewer.1995@gmail.com>
2023-11-19 15:38:29 -0500

    added variables to get data from local storage

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,16 @@ $(function () {
 
     localStorage.setItem(description, time);
   })
+  // variables to get data from local storage
+  $("#hour-9 .description").val(localStorage.getItem("hour9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour17"));
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
